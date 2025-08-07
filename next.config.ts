0f57a001a181cfeb5bfa5b59314a92b092045322
Furlight <corentin.houssais0@gmail.com>
2025-08-07 19:33:10 +0200

    commit add update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,30 @@
 import type { NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/",
       },
-      // Si tienes otros dominios, mantenerlos aquí
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      }
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/",
+      },
     ],
   },
   // ...otras configuraciones existentes...
 };
 
-export default nextConfig;
+export default nextConfig;
